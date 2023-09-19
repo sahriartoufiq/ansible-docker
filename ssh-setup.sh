a917564aa1601433d1b7fd0d32b1ecf80b7ff810
sahriartoufiq <sahriartoufiq@gmail.com>
2023-09-20 01:17:07 +0600

    Add variables

--- ssh-setup.sh
+++ ssh-setup.sh
@@ -2,10 +2,10 @@
 
 ssh-keygen -t rsa -b 2048 -C "your_email@example.com" -f ansible_keys -N ""
 
-docker build -t centos-ssh .
-docker run -d --name centos-ssh-container centos-ssh
+docker build -t centos-ssh2 .
+docker run -d --name centos-ssh-container2 centos-ssh2
 
-container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' centos-ssh-container)
+container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' centos-ssh-container2)
 
 echo "[tomcat-servers]" > inventory.ini
 echo $container_ip >> inventory.ini
